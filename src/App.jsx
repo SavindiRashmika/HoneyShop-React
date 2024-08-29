7069ac25adcd55f5d6ed791c576e8ae54b672e18
SavindiRashmika <rashmikawadanambi>
2024-08-29 15:29:08 +0530

    nav bar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
-    <>
-      <div className='bg-[#171d32] h-auto w-full overflow-hidden'>
+    
+      <div className='bg-[#171d32] h-auto w-full overflow-hidden p-0'>
+        <Navbar/>
       </div>
-    </>
-  )
+     )
 }
 
 export default App
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
+import { BsCart2 } from "react-icons/bs";
+import { MdHome } from "react-icons/md";
+import { FaSearch } from "react-icons/fa";
+
 
 function Navbar() {
   return (
-    <nav className='flex flex-wrap justify-between md:items-center text-white px-10 pt-6
+    <nav className='flex flex-wrap justify-between md:items-center text-white px-10 pt-6 bg-origin-padding
     md:px-20'>
-        <span>Honey</span>
-        <ul>
-            <li>Home</li>
-            <li>About us</li>
-            <li>Our Shop</li>
-            <li>Our Customer</li>
-            <li>Contact Us</li>
-        </ul>
+      <span className='text-3xl font-semibold tracking-wide flex gap-3'><MdHome size={38}  color="#ff9f0d"/>Honey</span>
+      <ul className='mx-24 text-xl py-2 mb-3 font-semibold md:mt-5 px-2 rounded-xl bg-opacity-30 md:border-none text-center md:bg-transparent md:static md:mx-0 md:flex gap-6'>
+        <li className='text-xs transition-all duration-300 p-1 md:p-0'>Home</li>
+        <li className='text-xs transition-all duration-300 p-1 md:p-0'>About us</li>
+        <li className='text-xs transition-all duration-300 p-1 md:p-0'>Our Shop</li>
+        <li className='text-xs transition-all duration-300 p-1 md:p-0'>Our Customer</li>
+        <li className='text-xs transition-all duration-300 p-1 md:p-0'>Contact Us</li>
+      </ul>
+      <div className='flex gap-5 items-center' style={{ backgroundColor: '#ff9f0d', padding: '8px 15px', borderRadius: '3rem',color:'black' }}>
+        <FaSearch size={24}/>
+        <BsCart2 size={28}/>
+      </div>
     </nav>
   )
 }
