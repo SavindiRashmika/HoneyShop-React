6936c60223c4551f591327ea59839c389f24c955
SavindiRashmika <rashmikawadanambi>
2024-09-02 23:47:41 +0530

    products page added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,27 +3,27 @@ import React from 'react'
 function About() {
   return (
     <div className='flex flex-row p-10 md:p-20'>
-        <div>
-        <img src="src/assets/img/about.jpg" alt="Hero" className='w-10/12 h-1/3 rounded-lg rotate-[-16deg]'/>
-        <img src="src/assets/img/R.jpeg" alt="Hero" className='w-9/12 h-1/3 bg-contain rounded-lg rotate-[-16deg]'/>
+      <div className='pl-36'>
+        <img src="src/assets/img/about.jpg" alt="Hero" className='w-8/12 rounded-lg rotate-[-16deg]' />
+        <img src="src/assets/img/R.jpeg" alt="Hero" className='w-8/12 h-60 bg-contain rounded-lg rotate-[-16deg]' />
+      </div>
 
-        </div>
-        <div className='text-[#9e9e9e] ml-5 leading-7'>
-            <h1 className='text-5xl font-bold mb-12 text-white'>Crafting the Perfect Honey Experience</h1>
-            <p >Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-               Voluptatum cum quisquam in consectetur minus ducimus nihil 
-               est modi culpa. Porro fuga voluptates nam veniam unde aperiam 
-               qui atque ex libero?</p><br/>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-               Nam quaerat labore amet quam a, id tempore iste voluptates consectetur 
-               enim velit beatae repellendus alias, excepturi cum praesentium quis quod sit!</p><br/>
-               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-               Nam quaerat labore amet quam!</p>
+      <div className=' text-[#9e9e9e] ml-5 leading-7'>
+        <h1 className='text-5xl font-bold mb-12 text-white'>Crafting the Perfect Honey Experience</h1>
+        <p >Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Voluptatum cum quisquam in consectetur minus ducimus nihil
+          est modi culpa. Porro fuga voluptates nam veniam unde aperiam
+          qui atque ex libero?</p><br />
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Nam quaerat labore amet quam a, id tempore iste voluptates consectetur
+          enim velit beatae repellendus alias, excepturi cum praesentium quis quod sit!</p><br />
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Nam quaerat labore amet quam!</p>
 
-               <div className='mt-14'>
-        <a href="#" className="bg-[#111111] text-[#ff9f0d] px-8 py-3 rounded-full mr-5 font-medium text-lg">Explore Menu </a>
+        <div className='mt-14'>
+          <a href="#" className="bg-[#111111] text-[#ff9f0d] px-8 py-3 rounded-full mr-5 font-medium text-lg">Explore Menu </a>
         </div>
-        </div>
+      </div>
     </div>
   )
 }
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,23 +2,22 @@ import React from 'react'
 
 function Products() {
   return (
-    <div>
-        <div>
-            <h4>Our Shop</h4>
-            <h2>Lets Check Popular Products</h2>
+    <div className='p-10 md:p-20'>
+        <div className='flex flex-col text-center'>
+            <h4 className='text-[#ff9f0d] font-semibold text-xl mb-3'>Our Shop</h4>
+            <h2 className='text-white font-bold text-4xl mb-10'>Lets Check Popular Products</h2>
         </div>
 
-        <div>
-            <div>
-            <img src="src/assets/img/R.jpeg" alt="Hero" className='w-full max-w-60 h-auto'/>
+        <div className='grid grid-flow-col gap-4'>
+            <div className='bg-gradient-to-r from-slate-950 to-black py-8 px-5 text-center text-white rounded-3xl'>
+            <img src="src/assets/img/R.jpeg" alt="Hero" className='w-full max-w-60 h-auto p-3 rounded-3xl'/>
                 <h3>Blueberry Honey</h3>
-                <p>It is a long established fact that a reader</p>
                 <div>
-                    <div>
+                    <div className='mb-3 font-bold text-xl'>
                         <h6>$21.00</h6>
                     </div>
                     <div>
-                        <a href="#">Order now</a>
+                        <a href="#" className="bg-[#ff9f0d] text-[#111111] px-4 py-2 rounded-full mr-5 font-bold text-sm">Order now</a>
                     </div>
                 </div>
 
@@ -27,16 +26,15 @@ function Products() {
                 </div>
             </div>
 
-            <div>
-            <img src="src/assets/img/R.jpeg" alt="Hero" className='w-full max-w-60 h-auto'/>
+            <div className='bg-gradient-to-r from-slate-950 to-black py-8 px-5 text-center text-white rounded-3xl'>
+            <img src="src/assets/img/p3.png" alt="Hero" className='w-full max-w-60 h-auto'/>
                 <h3>Blueberry Honey</h3>
-                <p>It is a long established fact that a reader</p>
                 <div>
-                    <div>
+                    <div className='mb-3 font-bold text-xl'>
                         <h6>$21.00</h6>
                     </div>
                     <div>
-                        <a href="#">Order now</a>
+                        <a href="#" className="bg-[#ff9f0d] text-[#111111] px-4 py-2 rounded-full  font-bold text-sm">Order now</a>
                     </div>
                 </div>
 
@@ -45,34 +43,32 @@ function Products() {
                 </div>
             </div>
 
-            <div>
+            <div className='bg-gradient-to-r from-slate-950 to-black py-8 px-5 text-center text-white rounded-3xl'>
             <img src="src/assets/img/R.jpeg" alt="Hero" className='w-full max-w-60 h-auto'/>
                 <h3>Blueberry Honey</h3>
-                <p>It is a long established fact that a reader</p>
                 <div>
-                    <div>
+                    <div className='mb-3 font-bold text-xl'>
                         <h6>$21.00</h6>
                     </div>
                     <div>
-                        <a href="#">Order now</a>
+                        <a href="#" className="bg-[#ff9f0d] text-[#111111] px-4 py-2 rounded-full mr-5 font-bold text-sm">Order now</a>
                     </div>
                 </div>
 
                 <div>
-                    <a href="#"><i class="bx bx-heart"></i></a>
+                    <a href="#" ><i class="bx bx-heart"></i></a>
                 </div>
             </div>
 
-            <div>
-            <img src="src/assets/img/R.jpeg" alt="Hero" className='w-full max-w-60 h-auto'/>
+            <div className='bg-gradient-to-r from-slate-950 to-black py-8 px-5 text-center text-white rounded-3xl'>
+            <img src="src/assets/img/p2.png" alt="Hero" className='w-full max-w-60 h-auto'/>
                 <h3>Blueberry Honey</h3>
-                <p>It is a long established fact that a reader</p>
-                <div>
-                    <div>
+                <div className='flex flex-col'>
+                    <div className='mb-3 font-bold text-xl'>
                         <h6>$21.00</h6>
                     </div>
                     <div>
-                        <a href="#">Order now</a>
+                        <a href="#" className="bg-[#ff9f0d] text-[#111111] px-4 py-2 rounded-full mr-5 font-bold text-sm">Order now</a>
                     </div>
                 </div>
 
@@ -82,7 +78,7 @@ function Products() {
             </div>
         </div>
 
-        <div>
+        <div className='text-center mt-16'>
             <a href="#" className="bg-[#111111] text-[#ff9f0d] px-8 py-3 rounded-full mr-5 font-medium text-lg">All Products</a>
         </div>
     </div>
